4252f97f8dfa149511350fa9d6795fe8040ff52c
nat kawakami <kawakami.v@aluno.ifsp.edu.br>
2026-08-25 21:43:11 -0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,27 +1,56 @@
-from flask import Flask, render_template, session, redirect, url_for, flash, request
-from flask_bootstrap import Bootstrap, url_for
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jssb0512nt1915'
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Iniciando extensões
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class Form(FlaskForm):
-  name = StringField('What is your name?', validators= [DataRequired()])
-  lastname = StringField('What is your last name?', validators= [DataRequired()])
-  institution = StringField('What is your institution?', validators= [DataRequired()])
-  discipline = SelectField('What is your discipline?', choices=[('DSWA5', 'DSWA5'), ('DSBA4', 'DSBA4'), ('Gestão de projetos', 'Gestão de projetos')], validators= [DataRequired()])
 
-  submit = SubmitField('Submit')
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -32,45 +61,20 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-# RAÍZ
+
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
-  form = Form()
-  if form.validate_on_submit():
-    old_name = session.get('name')
-    if old_name is not None and old_name != form.name.data:
-      flash('Looks like you have changed your name!')
-    session['name'] = form.name.data
-    session['lastname'] = form.lastname.data
-    session['institution'] = form.institution.data
-    session['discipline'] = form.discipline.data
-    return redirect(url_for('index'))
-  return render_template(
-      'index.html',
-      form=form,
-      name=session.get('name'),
-      lastname=session.get('lastname'),
-      institution=session.get('institution'),
-      discipline=session.get('discipline'),
-        ip=request.remote_addr,
-        host=request.host,
-      current_time=datetime.utcnow(),
-  )
-
-
-
-# # IDENTIFICAÇÃO   
-# @app.route('/user/<name>')
-# @app.route('/user/<name>/<prontuario>')
-# @app.route('/user/<name>/<prontuario>/<instituicao>')
-# def user(name, prontuario='PT3036413', instituicao='IFSP'):
-#     return render_template('id.html', name=name, prontuario=prontuario, instituicao=instituicao)
-
-# REQUISIÇÃO DE CONTEXTO
-# @app.route('/contextorequisicao/<name>')
-# def contextorequisicao(name):
-#     navegador = request.headers.get('User-Agent')
-#     ip_cliente = request.remote_addr
-#     host_app = request.host
-#     return render_template('contexto.html', name=name, navegador=navegador, ip_cliente=ip_cliente, host_app=host_app)
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
