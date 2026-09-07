f6e0fddd8fa33b7c3669dc1ddcadbbf502f44635
logkawa <kawakami.v@aluno.ifsp.edu.br>
2026-09-07 20:58:36 -0300

    aula 08

--- README.md
+++ README.md
@@ -1,41 +1,16 @@
-# Aula 060. Bancos de dados
+# Desenvolvimento web com flask
 
-## Slide 21
-- from hello import db
-- db.create_all()
+## Preparando o ambiente
+-> Criar Venv: python -m venv venv
+-> work on venv: .\venv\Scripts\Activate.ps1
+-> Baixar arquivos: pip install -r requirements/common.txt
 
-## Slide 22
-- db.drop_all()
-- db.create_all()
+## Criando DB
+-> variável de acesso: $env:FLASK_APP = "hello.py"
+    • flask shell
+    • db.create_all()
+    • exit()
+[setattr(u, 'role', Role.query.filter_by(name='User').first()) for u in User.query.filter_by(role_id=None).all()]; db.session.commit()
 
-## Slide 23
-- from hello import Role, User
-- admin_role = Role(name='Admin')
-- mod_role = Role(name='Moderator')
-- user_role = Role(name='User')
-- user_john = User(username='john', role=admin_role)
-- user_susan = User(username='susan', role=user_role)
-- user_david = User(username='david', role=user_role)
-
-## Slide 24
-- db.session.add_all([admin_role, mod_role, user_role, user_john, user_susan, user_david])
-
-## Slide 25
-- db.session.commit()
-
-## Slide 26
-- admin_role.name = 'Administrator'
-- db.session.add(admin_role)
-- db.session.commit()
-
-## Slide 27
-- db.session.delete(mod_role)
-- db.session.commit()
-
-## Slide 28
-- Role.query.all()
-- User.query.all()
-- User.query.filter_by(role=user_role).all()
-
-## Slide 30
-- user_role = Role.query.filter_by(name='User').first()
+## Iniciando localmente
+-> flask --app hello run --debug
--- hello.py
+++ hello.py
@@ -1,80 +1,170 @@
+# --- IMPORTAÇÕES DO FLASK ---
+# region
+
+# --- Bibliotecas padrão Python ---
 import os
-from flask import Flask, render_template, session, redirect, url_for
+from datetime import datetime
+
+# --- Núcleo Flask ---
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+
+# --- Interface e utilidades ---
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+
+# --- Formulários e Validações ---
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
+
+# --- Banco de dados ---
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
+# endregion
+
+
+
+
+
+# --- CONFIGURAÇÕES BÁSICAS ---
+# region
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SECRET_KEY'] = 'chaveultrasecreta' # Segurança contra CSRF
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# endregion
+
+
+
+
+
+# --- INICIANDO EXTENSÕES ---
+# region
+
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+# endregion
 
 
+
+
+
+# --- CLASSES WTFORMS ---
+# region
+
+class NameForm(FlaskForm):
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+#endregion
+
+
+
+
+
+# --- MODELOS DB SQLALCHEMY ---
+# region
+
+# --- Cargos ---
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic') # Uma Role pode estar ligada a vários Users
 
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return f'<Role {self.name}>'
 
-
+# --- Usuários ---
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) # FK apontando para o id da tabela roles
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.username}>'
+    
+# endregion
 
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+
+# --- CONTEXTOS GLOBAIS ---
+# region
+
+# --- Relógio global ---
+@app.context_processor
+def inject_time():
+    return dict(current_time=datetime.utcnow())
+
+#endregion
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
 
+# --- ROTAS ---
+# region
 
+# --- Rota Raíz --- 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            # Busca a role padrão 'User' no banco de dados
+            user_role = Role.query.filter_by(name='User').first()
+            
+            # Cria o novo usuário vinculando-o à role padrão
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
+            
             session['known'] = False
         else:
             session['known'] = True
+            
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    
+    # Consulta todos os usuários para preencher a tabela HTML
+    todos_os_usuarios = User.query.all()
+    
+    return render_template('index.html',
+                           form=form,
+                           nome_completo=session.get('name'),
+                           known=session.get('known', False),
+                           users=todos_os_usuarios)
+
+# endregion
+
+
+
+
+
+# --- ERROR HANDLING ---
+# region
+
+# --- ERRO 404 ---
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# --- ERRO 500 - SERVIDOR ---
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+# endregion
+
+# --- SERVIDOR LOCAL ---
+if __name__ == '__main__':
+    app.run(debug=True)
