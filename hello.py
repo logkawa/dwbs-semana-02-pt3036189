eaa7ab1e5e927ce2a8c6f81fcc3815a79b778ec0
nat kawakami <kawakami.v@aluno.ifsp.edu.br>
2026-09-20 01:06:35 +0000

    semana 10

--- hello.py
+++ hello.py
@@ -5,8 +5,14 @@
 import os
 from datetime import datetime
 
+# --- Requisições HTTP ---
+import requests
+
+# --- Variáveis de ambiente ---
+from dotenv import load_dotenv
+
 # --- Núcleo Flask ---
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash
 
 # --- Interface e utilidades ---
 from flask_bootstrap import Bootstrap
@@ -14,7 +20,7 @@ from flask_moment import Moment
 
 # --- Formulários e Validações ---
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 # --- Banco de dados ---
@@ -24,22 +30,35 @@ from flask_migrate import Migrate
 # endregion
 
 
-
-
-
 # --- CONFIGURAÇÕES BÁSICAS ---
 # region
 
+# Carrega as variáveis do arquivo .env
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'chaveultrasecreta' # Segurança contra CSRF
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+# --- Variáveis de ambiente ---
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['FLASKY_APP'] = os.environ.get('FLASKY_APP')
+
+app.config['API_URL'] = os.environ.get('API_URL')
+app.config['API_KEY'] = os.environ.get('API_KEY')
+app.config['API_FROM'] = os.environ.get('API_FROM')
+
+# --- Chave secreta ---
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+# --- Banco de dados ---
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # endregion
-
-
-
 
 
 # --- INICIANDO EXTENSÕES ---
@@ -53,129 +72,232 @@ migrate = Migrate(app, db)
 # endregion
 
 
-
-
-
-# --- CLASSES WTFORMS ---
+# --- CLASSE DO FORMULÁRIO ---
 # region
 
 class NameForm(FlaskForm):
-    name = StringField('Qual o seu nome?', validators=[DataRequired()])
-    role = SelectField('Qual a sua função?', coerce=int)
+
+    nome = StringField(
+        'Qual o seu nome?',
+        validators=[DataRequired()]
+    )
+
+    prontuario = StringField(
+        'Qual o seu prontuário?',
+        validators=[DataRequired()]
+    )
+
+    username = StringField(
+        'Qual o seu usuário?',
+        validators=[DataRequired()]
+    )
+
     submit = SubmitField('Enviar')
 
-#endregion
 
-
-
-
-
-# --- MODELOS DB SQLALCHEMY ---
-# region
-
-# --- Cargos ---
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic') # Uma Role pode estar ligada a vários Users
-
-    def __repr__(self):
-        return f'<Role {self.name}>'
-
-# --- Usuários ---
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) # FK apontando para o id da tabela roles
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-    
 # endregion
 
 
+# --- MODELO DO BANCO DE DADOS ---
+# region
 
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    nome = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    prontuario = db.Column(
+        db.String(20),
+        unique=True,
+        nullable=False
+    )
+
+    username = db.Column(
+        db.String(100),
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+# endregion
+
+
+# --- ENVIO DE E-MAIL ---
+# region
+
+def enviar_email_cadastro(user):
+
+    api_url = app.config['API_URL']
+    api_key = app.config['API_KEY']
+    email_from = app.config['API_FROM']
+    email_admin = app.config['FLASKY_ADMIN']
+
+    dados = {
+        'from': email_from,
+
+        # O usuário é considerado o endereço institucional
+        'to': [
+            email_admin,
+            "flaskaulasweb@zohomail.com"
+        ],
+
+        'subject': 'Novo usuário cadastrado',
+
+        'text': f"""
+Novo usuário cadastrado.
+
+Prontuário: {user.prontuario}
+Nome do aluno: {user.nome}
+Usuário: {user.username}
+"""
+    }
+
+    resposta = requests.post(
+        api_url,
+        auth=('api', api_key),
+        data=dados
+    )
+
+    if resposta.status_code == 200:
+        return True
+
+    print('Erro ao enviar e-mail:')
+    print(resposta.status_code)
+    print(resposta.text)
+
+    return False
+
+
+# endregion
 
 
 # --- CONTEXTOS GLOBAIS ---
 # region
 
-# --- Relógio global ---
 @app.context_processor
 def inject_time():
-    return dict(current_time=datetime.utcnow())
 
-#endregion
+    return dict(
+        current_time=datetime.utcnow()
+    )
 
 
-
+# endregion
 
 
 # --- ROTAS ---
 # region
 
-# --- Rota Raíz --- 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     form = NameForm()
-    
-    # Preenche o dropdown com as roles no banco de dados
-    form.role.choices = [(r.id, r.name) for r in Role.query.order_by(Role.name).all()]
-    
+
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        
+
+        # Verifica se o usuário já existe
+        user = User.query.filter_by(
+            username=form.username.data
+        ).first()
+
         if user is None:
-            selected_role = Role.query.get(form.role.data)
-            
-            user = User(username=form.name.data, role=selected_role)
+
+            # Cria o novo usuário
+            user = User(
+                nome=form.nome.data,
+                prontuario=form.prontuario.data,
+                username=form.username.data
+            )
+
+            # Salva no banco
             db.session.add(user)
             db.session.commit()
-            
+
+            # Envia o e-mail
+            email_enviado = enviar_email_cadastro(user)
+
+            if email_enviado:
+
+                flash(
+                    'Usuário cadastrado e e-mail enviado com sucesso!',
+                    'success'
+                )
+
+            else:
+
+                flash(
+                    'Usuário cadastrado, mas ocorreu um erro ao enviar o e-mail.',
+                    'warning'
+                )
+
             session['known'] = False
+
         else:
+
             session['known'] = True
-            
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    
+
+            flash(
+                'Este usuário já está cadastrado.',
+                'warning'
+            )
+
+        session['name'] = form.nome.data
+
+        return redirect(
+            url_for('index')
+        )
+
+    # Usuários cadastrados
     todos_os_usuarios = User.query.all()
-    todas_as_funcoes = Role.query.all()
-    
-    return render_template('index.html',
-                           form=form,
-                           nome_completo=session.get('name'),
-                           known=session.get('known', False),
-                           users=todos_os_usuarios,
-                           roles=todas_as_funcoes)
+
+    return render_template(
+        'index.html',
+        form=form,
+        nome_completo=session.get('name'),
+        known=session.get('known', False),
+        users=todos_os_usuarios
+    )
+
 
 # endregion
-
-
-
 
 
 # --- ERROR HANDLING ---
 # region
 
-# --- ERRO 404 ---
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
 
-# --- ERRO 500 - SERVIDOR ---
+    return render_template(
+        '404.html'
+    ), 404
+
+
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+
+    return render_template(
+        '500.html'
+    ), 500
+
 
 # endregion
 
 
-
-
-
 # --- SERVIDOR LOCAL ---
 if __name__ == '__main__':
+
     app.run(debug=True)
